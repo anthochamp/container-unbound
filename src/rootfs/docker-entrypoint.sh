a106c6aab3ce055dff163912a507f09b3e271e9d
Anthony Champagne <dev@anthonychampagne.fr>
2025-02-09 01:55:20 +0100

    docker-entrypoint: lint

--- src/rootfs/docker-entrypoint.sh
+++ src/rootfs/docker-entrypoint.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env sh
 set -eu
 
-# shellcheck disable=SC2120
+# shellcheck disable=SC2120,SC3043
 replaceEnvSecrets() {
-  # replaceEnvSecrets 1.0.0
-  local prefix="${1:-}"
+	# replaceEnvSecrets 1.0.0
+	# https://gist.github.com/anthochamp/d4d9537f52e5b6c42f0866dd823a605f
+	local prefix="${1:-}"
 
-  for envSecretName in $(export | awk '{print $2}' | grep -oE '^[^=]+' | grep '__FILE$'); do
-    if [ -z "$prefix" ] || printf '%s' "$envSecretName" | grep "^$prefix" >/dev/null; then
-      local envName
-      envName=$(printf '%s' "$envSecretName" | sed 's/__FILE$//')
+	for envSecretName in $(export | awk '{print $2}' | grep -oE '^[^=]+' | grep '__FILE$'); do
+		if [ -z "$prefix" ] || printf '%s' "$envSecretName" | grep "^$prefix" >/dev/null; then
+			local envName
+			envName=$(printf '%s' "$envSecretName" | sed 's/__FILE$//')
 
-      local filePath
-      filePath=$(eval echo '${'"$envSecretName"':-}')
+			local filePath
+			filePath=$(eval echo '${'"$envSecretName"':-}')
 
-      if [ -n "$filePath" ]; then
-        if [ -f "$filePath" ]; then
-          echo Using content from "$filePath" file for "$envName" environment variable value.
+			if [ -n "$filePath" ]; then
+				if [ -f "$filePath" ]; then
+					echo Using content from "$filePath" file for "$envName" environment variable value.
 
-          export "$envName"="$(cat -A "$filePath")"
-          unset "$envSecretName"
-        else
-          echo ERROR: Environment variable "$envSecretName" is defined but does not point to a regular file. 1>&2
-          exit 1
-        fi
-      fi
-    fi
-  done
+					export "$envName"="$(cat -A "$filePath")"
+					unset "$envSecretName"
+				else
+					echo ERROR: Environment variable "$envSecretName" is defined but does not point to a regular file. 1>&2
+					exit 1
+				fi
+			fi
+		fi
+	done
 }
 
 replaceEnvSecrets UNBOUND_
@@ -46,12 +47,12 @@ export UNBOUND_NEG_CACHE_SIZE="${UNBOUND_NEG_CACHE_SIZE:-}"
 UNBOUND_CACHE_SIZE_HINT=${UNBOUND_CACHE_SIZE_HINT:-}
 
 if [ -n "$UNBOUND_CACHE_SIZE_HINT" ]; then
-  export UNBOUND_MSG_CACHE_SIZE="${UNBOUND_CACHE_SIZE_HINT}"m
-  # https://unbound.docs.nlnetlabs.nl/en/latest/topics/core/performance.html#configuration
-  export UNBOUND_RRSET_CACHE_SIZE=$((UNBOUND_CACHE_SIZE_HINT * 2))m
+	export UNBOUND_MSG_CACHE_SIZE="${UNBOUND_CACHE_SIZE_HINT}"m
+	# https://unbound.docs.nlnetlabs.nl/en/latest/topics/core/performance.html#configuration
+	export UNBOUND_RRSET_CACHE_SIZE=$((UNBOUND_CACHE_SIZE_HINT * 2))m
 
-  export UNBOUND_KEY_CACHE_SIZE="${UNBOUND_CACHE_SIZE_HINT}"m
-  export UNBOUND_NEG_CACHE_SIZE=$((UNBOUND_CACHE_SIZE_HINT / 4))m
+	export UNBOUND_KEY_CACHE_SIZE="${UNBOUND_CACHE_SIZE_HINT}"m
+	export UNBOUND_NEG_CACHE_SIZE=$((UNBOUND_CACHE_SIZE_HINT / 4))m
 fi
 
 j2Templates="
@@ -59,11 +60,11 @@ j2Templates="
 "
 
 for file in $j2Templates; do
-  jinja2 -o "$file" "$file.j2"
+	jinja2 -o "$file" "$file.j2"
 
-  # can't use --reference with alpine
-  chmod "$(stat -c '%a' "$file.j2")" "$file"
-  chown "$(stat -c '%U:%G' "$file.j2")" "$file"
+	# can't use --reference with alpine
+	chmod "$(stat -c '%a' "$file.j2")" "$file"
+	chown "$(stat -c '%U:%G' "$file.j2")" "$file"
 done
 
 # update dynamic config owner/group in case it is mounted
