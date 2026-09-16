93f1b5d45999172ed8afcb9e6b1206dcb7a95d8d
Anthony Champagne <dev@anthonychampagne.fr>
2026-09-17 01:09:39 +0200

    raise test hookTimeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineProject } from "@ac-kit/vitest-config";
 export default defineProject({
 	test: {
 		testTimeout: 30_000,
-		hookTimeout: 60_000,
+		hookTimeout: 300_000, // container's build from sources
 		isolate: true,
 		fileParallelism: false,
 		sequence: {
